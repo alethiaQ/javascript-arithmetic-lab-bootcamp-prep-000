1c5dad731f1e01c0ea8ae978d4adb3012a129317
Alethia Quintero <alethiaq22@gmail.com>
2019-06-07 23:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function add(a,b){
+function add(a,b) {
   return a + b 
 }
-function subtract(a,b){
+function subtract(a,b) {
   return a - b 
 }
-function multiply(a,b){
+function multiply(a,b) {
   return a * b 
 }
-function divide(a,b){
+function divide(a,b) {
   return a/b
 }
 function inc(n) {
   return n+= 1
 }
-function dec(n){
+function dec(n) {
   return n-= 1;
 }
-function makeInt(n){
- parseInt(n, 10)
+function makeInt(n) {
+ parseInt('n', 10)
  return n
 }
-function preserveDecimal(n){
-parseFloat(n)
+function preserveDecimal(n) {
+parseFloat('n')
 return n
 }
